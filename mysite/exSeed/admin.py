f71b348ad8d3e8d9fc163593ef4b97e8c73e358c
rexehunin <rhn202@exeter.ac.uk>
2023-02-25 02:29:06 +0000

    Updated database schema to fit spot dev's desire (Ben) (Added new avatar model to store avatar data, description box to spots, altered spot register to now reference the spot record instance instead of the spot itself)

--- mysite/exSeed/admin.py
+++ mysite/exSeed/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import UserInfo, Spot, CurrentSpotRegister,PreviousSpotAttend
+from .models import UserInfo, Spot, SpotRegister, PreviousSpotAttend, Avatar
 
 # Register your models here.
 admin.site.register(UserInfo)
 admin.site.register(Spot)
-admin.site.register(CurrentSpotRegister)
+admin.site.register(SpotRegister)
 admin.site.register(PreviousSpotAttend)
+admin.site.register(Avatar)
--- mysite/exSeed/models.py
+++ mysite/exSeed/models.py
@@ -10,14 +10,16 @@ class UserInfo(models.Model):
         help_text="Linking this information to the user in auth_User it relates to",
         on_delete=models.CASCADE,
     )
-    avatarId = models.PositiveSmallIntegerField(
-        help_text="The ID number of the users chosen avatar (range is from 0 to 32767)",
-        default=0,  # All users initially have the same default pfp
-        blank=True,
+    avatarId = models.ForeignKey(
+        'Avatar',
+        on_delete=models.CASCADE,
+        help_text="User's chosen avatar",
+        default=1,
     )
     title = models.CharField(
         help_text="The title chosen by the user to represent them",
         max_length=100,
+        blank=True,
         # ! Don't know how we are dealing with this. Is this the actual title, or just a reference to a title
     )
     totalPoints = models.PositiveSmallIntegerField(
@@ -42,6 +44,7 @@ class UserInfo(models.Model):
         max_length=2,
         choices=GROUP_CHOICES,
         null=True,
+        blank=True,
     )
 
     def __str__(self):
@@ -52,11 +55,30 @@ class UserInfo(models.Model):
         verbose_name = "Users Info"
 
 
+class Avatar(models.Model):
+    imageName = models.CharField(
+        help_text="The png name of the avatar image",
+        max_length=100,
+    )
+    avatarTitle = models.CharField(
+        help_text="The name found under the avatar in the selection menu",
+        max_length=100,
+    )
+
+    def __str__(self):
+        return self.imageName
+
+
 class Spot(models.Model):
     name = models.CharField(
         help_text="A spot's name",
         unique=True,  # Unique implies db_index = True
         max_length=100,
+    )
+    desc = models.TextField(
+        help_text="A description of the spot, to appear when a user sees the spot",
+        max_length=500,
+        null=True,  # Allows a spot to be created without needing a description at moment of creation
     )
     latitude = models.DecimalField(
         help_text="This spot's latitude co-ordinate (Lines parallel to the equator, ranging from 90 (North) to -90 ("
@@ -79,11 +101,11 @@ class Spot(models.Model):
     average_attendance_int = models.PositiveSmallIntegerField(  # This field supports a max value of 32767
         help_text="The average whole number of users that come to this spot when it is spot of the day",
         default=0,
-        db_index=True,  # This may be an important value through which to judge spots
+        #db_index=True,  # This may be an important value through which to judge spots
     )
-    image_pointer = models.PositiveSmallIntegerField(
-        help_text="The ID number of the spot's saved image (range is from 0 to 32767)",
-        default=0,  # default value when spot is created
+    imageName = models.CharField(
+        help_text="The name of the png file that holds this spots image",
+        max_length=100,
         blank=True,
     )
 
@@ -95,23 +117,24 @@ class Spot(models.Model):
         verbose_name_plural = "Spots"
 
 
-class CurrentSpotRegister(models.Model):
-    uId = models.OneToOneField(
+class SpotRegister(models.Model):
+    uId = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         help_text="This user has attended today's spot",
     )
-    sId = models.ForeignKey(
-        'Spot',
+    psaId = models.ForeignKey(
+        'PreviousSpotAttend',
         on_delete=models.CASCADE,
-        help_text="Today's spot",
+        verbose_name="Spot data",
+        help_text="Related spot-day instance"
     )
 
     def __str__(self):
-        return str(self.uId)
+        return str(self.uId.username) + " attended the spot on " + str(self.psaId.spotDay)
 
     class Meta:
-        verbose_name_plural = "Daily Register"
+        verbose_name_plural = "Register"
         verbose_name = "Entries"
 
 
@@ -128,8 +151,12 @@ class PreviousSpotAttend(models.Model):
         help_text="The day that this was the spot of the day, represented as the Python datetime.date value",
         unique=True,  # This implies db_index = True
         null=True,
+        db_index=True,  # Orders entries by date submitted
     )
 
+    def __str__(self):
+        return str(self.spotDay) + " " + self.sId.name
+
     class Meta:
-        verbose_name_plural = "Previous Spot Attendance Record"
-        verbose_name = "Previous Spot Attendance Records"
+        verbose_name_plural = "Spot Record"
+        verbose_name = "Spot Records"
