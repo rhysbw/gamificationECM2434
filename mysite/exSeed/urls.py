4e687b0beeaf04ee7218b99d6ec2fd0ca881e5ab
rexehunin <rhn202@exeter.ac.uk>
2023-02-27 19:09:44 +0000

    Leaderboard now works with rank values

--- mysite/exSeed/urls.py
+++ mysite/exSeed/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 from . import views
+import re
 
 urlpatterns = [
     path("login/", views.login_request, name="login"),
@@ -8,6 +9,5 @@ urlpatterns = [
     path("logout", views.logout_request, name="logout"),
     path("delete/<username>", views.delete_request, name='delete'),
     path('', views.home_page, name="home"),
-    path("leaderboard/streak", views.leaderboard, name="lb_streak"),
-    path("leaderboard/total", views.leaderboard, name="lb_total")
+    path("leaderboard", views.leaderboard, name="leaderboard")
 ]
